4211cdf861e924e73201ac22108edf376801c63b
ITcacenco <ivan.tcacenco@endava.com>
2022-03-23 12:30:15 +0200

    Implement HashMap collection for work with the Student class

--- src/main/java/com/endava/internship/collections/StudentMap.java
+++ src/main/java/com/endava/internship/collections/StudentMap.java
@@ -1,78 +1,322 @@
 package com.endava.internship.collections;
 
+import java.util.AbstractCollection;
+import java.util.AbstractSet;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Set;
 
 public class StudentMap implements Map<Student, Integer> {
+
+    private final Pair<Student, Integer>[] buckets;
+    private static final int INITIAL_CAPACITY = 1 << 4;
+    private int size = 0;
+
+    public StudentMap() {
+        this(INITIAL_CAPACITY);
+    }
+
+    public StudentMap(final int capacity) {
+        this.buckets = new Pair[capacity];
+    }
+
+    static class Pair<K, V> {
+        private final K key;
+        private V value;
+        private final Pair<K, V> next;
+
+        public Pair(final K key, final V value, final Pair<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public Pair<K, V> getNext() {
+            return next;
+        }
+
+        @Override
+        public String toString() {
+            return key.toString() + value.toString();
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            var pair = (Pair<?, ?>) o;
+            return Objects.equals(key, pair.key) && Objects.equals(value, pair.value);
+        }
+    }
+
     @Override
     public int size() {
-        //TODO
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        //TODO
+        return 0 == size;
+    }
+
+    @Override
+    public boolean containsKey(final Object o) {
+        if (!(o instanceof Student)) {
+            return false;
+        }
+        final var index = Math.abs(o.hashCode()) % buckets.length;
+
+        var existing = buckets[index];
+        if (null == existing) {
+            return false;
+        } else {
+            do {
+                if (existing.key.equals(o)) {
+                    return true;
+                }
+                existing = existing.next;
+            } while (null != existing.next);
+        }
         return false;
     }
 
     @Override
-    public boolean containsKey(Object o) {
-        //TODO
+    public boolean containsValue(final Object o) {
+        if (!(o instanceof Integer)) {
+            return false;
+        }
+
+        Pair<Student, Integer> pair;
+        for (var bucket : buckets) {
+            if (null == bucket) {
+                continue;
+            }
+            pair = bucket;
+            do {
+                if (o == pair.getValue()) {
+                    return true;
+                }
+                pair = pair.next;
+            } while (null != pair);
+        }
         return false;
     }
 
     @Override
-    public boolean containsValue(Object o) {
-        //TODO
-        return false;
-    }
+    public Integer get(final Object o) {
+        if (!(o instanceof Student)) {
+            return null;
+        }
+        var pair = buckets[Math.abs(o.hashCode()) % buckets.length];
 
-    @Override
-    public Integer get(Object o) {
-        //TODO
+        while (null != pair) {
+            if (pair.key.equals(o)) {
+                return pair.value;
+            }
+            pair = pair.next;
+        }
         return null;
     }
 
     @Override
-    public Integer put(Student student, Integer integer) {
-        //TODO
+    public Integer put(final Student student, final Integer integer) {
+        final var pair = new Pair<>(student, integer, null);
+
+        final var index = Math.abs(student.hashCode()) % buckets.length;
+
+        var existing = buckets[index];
+        if (null == existing) {
+            buckets[index] = pair;
+            size++;
+        } else {
+            do {
+                if (existing.key.equals(student)) {
+                    final var oldValue = existing.getValue();
+                    existing.value = integer;
+                    return oldValue;
+                }
+                existing = existing.next;
+            } while (null != existing.next);
+        }
         return null;
     }
 
     @Override
-    public Integer remove(Object o) {
-        //TODO
+    public Integer remove(final Object o) {
+        if (!(o instanceof Student)) {
+            return null;
+        }
+
+        final var index = Math.abs(o.hashCode()) % buckets.length;
+        var pair = buckets[index];
+
+        while (null != pair) {
+            if (pair.key.equals(o)) {
+                final var value = pair.value;
+                buckets[index] = pair.next;
+                size--;
+                return value;
+            }
+            pair = pair.next;
+        }
         return null;
     }
 
     @Override
-    public void putAll(Map<? extends Student, ? extends Integer> map) {
-        //TODO
+    public void putAll(final Map<? extends Student, ? extends Integer> map) {
+        for (var e : map.entrySet()) {
+            final var key = e.getKey();
+            final var value = e.getValue();
+            put(key, value);
+        }
     }
 
     @Override
     public void clear() {
-        //TODO
+        size = 0;
+        for (var i = 0; i < buckets.length; i++) {
+            if (null == buckets[i]) {
+                continue;
+            }
+            buckets[i] = null;
+        }
+
     }
 
     @Override
     public Set<Student> keySet() {
-        //TODO
-        return null;
+        return new KeySet();
+    }
+
+    final class KeySet extends AbstractSet<Student> {
+
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public void clear() {
+            StudentMap.this.clear();
+        }
+
+        public Iterator<Student> iterator() {
+            return new KeyIterator();
+        }
+
+        @Override
+        public boolean contains(final Object o) {
+            return containsKey(o);
+        }
+
+        @Override
+        public boolean remove(final Object key) {
+            return StudentMap.this.remove(key) != null;
+        }
+
     }
 
     @Override
     public Collection<Integer> values() {
-        //TODO
-        return null;
+        return new Values();
+    }
+
+    final class Values extends AbstractCollection<Integer> {
+
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public void clear() {
+            StudentMap.this.clear();
+        }
+
+        public Iterator<Integer> iterator() {
+            return new StudentMap.ValueIterator();
+        }
+
+        @Override
+        public boolean contains(final Object o) {
+            return containsValue(o);
+        }
+
     }
 
     @Override
     public Set<Entry<Student, Integer>> entrySet() {
         //Ignore this for homework
         throw new UnsupportedOperationException();
+    }
+
+    abstract class HashIterator {
+        StudentMap.Pair<Student, Integer> next;
+        StudentMap.Pair<Student, Integer> current;
+        int index;
+
+        HashIterator() {
+            var pairs = buckets;
+            current = next = null;
+            index = 0;
+            if (pairs != null && size > 0) {
+                while (index < pairs.length && null == (next = pairs[index])) {
+                    index++;
+                }
+            }
+        }
+
+        public final boolean hasNext() {
+            return next != null;
+        }
+
+        final StudentMap.Pair<Student, Integer> nextPair() {
+            var pairs = buckets;
+            var e = next;
+            if (e == null) {
+                throw new NoSuchElementException();
+            }
+            current = next;
+            next = current.next;
+            if (null == next && null != pairs) {
+                while (index < pairs.length && null == (next = pairs[index])) {
+                    index++;
+                }
+            }
+            return e;
+        }
+
+        public final void remove() {
+            var pair = current;
+            current = null;
+            StudentMap.this.remove(pair.key);
+        }
+    }
+
+    final class KeyIterator extends StudentMap.HashIterator implements Iterator<Student> {
+        public Student next() {
+            return nextPair().key;
+        }
+    }
+
+    final class ValueIterator extends StudentMap.HashIterator implements Iterator<Integer> {
+        public Integer next() {
+            return nextPair().value;
+        }
     }
 }
 
